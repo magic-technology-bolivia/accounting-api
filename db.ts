8194b64e7a133484d69f6e524893bb30c7a0958c
Smartisate Develop <smartisate.develop@gmail.com>
2023-08-01 18:16:49 -0400

    mongodb atlas

--- db.ts
+++ db.ts
@@ -2,7 +2,7 @@ const { connect } = require('mongoose');
 
 export const connectDB = async () => {
     try {
-        await connect('mongodb://localhost/taskdb');        
+        await connect('mongodb+srv://smartisatedevelop:S1stemas@cluster0.tdwpyqc.mongodb.net/?retryWrites=true&w=majority');        
         console.log('Mongodb connected');
     } catch (ex) {
         console.error(ex);
