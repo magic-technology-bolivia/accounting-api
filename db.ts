ece4d0be3d37781e792aaeed1b4e426ba8ed4dc4
Abel Pacara <abel.pacara@gmail.com>
2023-10-26 00:29:41 -0400

    db access modified

--- db.ts
+++ db.ts
@@ -4,7 +4,8 @@ dotenv.config();
 
 export const connectDB = async () => {
     try {
-        await connect(process.env.MONGO_URI ! );
+        //await connect(process.env.MONGO_URI ! );
+        await connect("mongodb+srv://smartisatedevelop:S1stemas@cluster0.tdwpyqc.mongodb.net/?retryWrites=true&w=majority");
         console.log('Mongodb connected');
     } catch (ex) {
         console.error(ex);
